562ff39caa57e86852b11243b81b2f668912fdf4
Michiel Borkent <michielborkent@gmail.com>
2025-09-25 11:13:50 +0200

    repro

--- Repro.java
+++ Repro.java
@@ -1,5 +1,23 @@
+import java.io.OutputStreamWriter;
+
 public class Repro {
+
+    static OutputStreamWriter out = null;
+
+    static void assignOutputWriter() {
+        out = new OutputStreamWriter(System.out);
+    }
+
+    static {
+        assignOutputWriter();
+    }
+
     public static void main(String [] args) {
-        System.out.println(System.getProperty("os.arch"));
+        System.out.println(System.getProperty("file.encoding"));
+        System.out.println(out.getEncoding());
+        System.out.println("λ");
+        assignOutputWriter();
+        System.out.println(out.getEncoding());
+        System.out.println("λ");
     }
 }
