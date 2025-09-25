eb2898d09d6389a56800e9837e5e7e563770099d
Michiel Borkent <michielborkent@gmail.com>
2025-09-25 19:31:14 +0200

    repro

--- Repro.java
+++ Repro.java
@@ -1,5 +1,7 @@
 public class Repro {
-    public static void main(String [] args) {
-        System.out.println(System.getProperty("os.arch"));
+    public static void main(String [] args) throws Exception {
+        System.out.println("env " + System.getenv("TMPDIR"));
+        System.out.println("prop " + System.getProperty("java.io.tmpdir"));
+        System.out.println("tmpfile " + java.io.File.createTempFile("clojure-lsp.", ".out"));
     }
 }
