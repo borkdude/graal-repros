1e12c158b66697d5d9529d810c445280c84717a3
Michiel Borkent <michielborkent@gmail.com>
2025-09-26 20:48:07 +0200

    print runtime encoding

--- src/my/repro.clj
+++ src/my/repro.clj
@@ -1,8 +1,11 @@
 (ns my.repro
   (:gen-class))
 
+(set! *warn-on-reflection* true)
+
 (defn -main [& _args]
   (prn (System/getProperty "java.file.encoding"))
+  (prn (.getEncoding ^java.io.OutputStreamWriter *out*))
   (prn "λ⚙️中文")
   (alter-var-root #'*out* (constantly (java.io.OutputStreamWriter. System/out)))
   (prn "λ⚙️中文"))
