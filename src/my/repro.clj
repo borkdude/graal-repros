bcd28ff5651bebe672c93ac0eecc59f06a6c844a
Michiel Borkent <michielborkent@gmail.com>
2025-09-26 20:44:44 +0200

    print encoding

--- src/my/repro.clj
+++ src/my/repro.clj
@@ -2,6 +2,7 @@
   (:gen-class))
 
 (defn -main [& _args]
+  (prn (System/getProperty "java.file.encoding"))
   (prn "λ⚙️中文")
   (alter-var-root #'*out* (constantly (java.io.OutputStreamWriter. System/out)))
   (prn "λ⚙️中文"))
