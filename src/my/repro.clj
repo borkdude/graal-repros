9b8018cab41166d0dc391d7112a2e2abfbc0e0cf
Michiel Borkent <michielborkent@gmail.com>
2025-10-02 10:42:23 +0200

    stdout.encoding

--- src/my/repro.clj
+++ src/my/repro.clj
@@ -5,6 +5,7 @@
 
 (defn -main [& _args]
   (prn (System/getProperty "java.file.encoding"))
+  (prn (System/getProperty "stdout.encoding"))
   (prn (.getEncoding ^java.io.OutputStreamWriter *out*))
   (prn "λ⚙️中文")
   (alter-var-root #'*out* (constantly (java.io.OutputStreamWriter. System/out)))
