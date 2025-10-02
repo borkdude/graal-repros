4d369fa03b5ee7ef62523cc53cb3d52c24f741f4
Michiel Borkent <michielborkent@gmail.com>
2025-10-02 10:49:42 +0200

    better print

--- src/my/repro.clj
+++ src/my/repro.clj
@@ -4,9 +4,10 @@
 (set! *warn-on-reflection* true)
 
 (defn -main [& _args]
-  (prn (System/getProperty "java.file.encoding"))
-  (prn (System/getProperty "stdout.encoding"))
-  (prn (.getEncoding ^java.io.OutputStreamWriter *out*))
-  (prn "λ⚙️中文")
+  (prn :java.file.encoding (System/getProperty "java.file.encoding"))
+  (prn :stdout.encoding (System/getProperty "stdout.encoding"))
+  (prn :out-var-encoding (.getEncoding ^java.io.OutputStreamWriter *out*))
+  (prn :unicode-string "λ⚙️中文")
+  (println "setting out var to new outputstreamwriter")
   (alter-var-root #'*out* (constantly (java.io.OutputStreamWriter. System/out)))
-  (prn "λ⚙️中文"))
+  (prn :unicode-string-with-new-out "λ⚙️中文"))
